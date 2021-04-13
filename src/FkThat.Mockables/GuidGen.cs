d3d010608a3ab41b9b4d789b3c8feedaf564effa
FkThat <fkthat@yandex.ru>
2021-04-13 05:57:50 +0300

    XML docs for GuidGen

--- src/FkThat.Mockables/GuidGen.cs
+++ src/FkThat.Mockables/GuidGen.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace FkThat.Mockables
 {
-    internal class GuidGen : IGuidGen
+    /// <summary>
+    /// GUID generator.
+    /// </summary>
+    public class GuidGen : IGuidGen
     {
+        /// <summary>
+        /// Initializes a new instance of the System.Guid structure.
+        /// </summary>
+        /// <returns>A new GUID object.</returns>
         public Guid NewGuid() => Guid.NewGuid();
     }
 }
--- src/FkThat.Mockables/IGuidGen.cs
+++ src/FkThat.Mockables/IGuidGen.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace FkThat.Mockables
 {
+    /// <summary>
+    /// GUID generator.
+    /// </summary>
     public interface IGuidGen
     {
+        /// <summary>
+        /// Initializes a new instance of the System.Guid structure.
+        /// </summary>
+        /// <returns>A new GUID object.</returns>
         Guid NewGuid();
     }
 }
