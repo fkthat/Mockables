f605ae7513aac5a6989de55e93c2610fa80941f3
fkthat <fkthat@yandex.ru>
2022-01-18 05:47:35 +0300

    Remove failed test

--- test/UnitTests.FkThat.Mockables/Test_SystemClock.cs
+++ test/UnitTests.FkThat.Mockables/Test_SystemClock.cs
@@ -25,11 +25,5 @@ namespace FkThat.Mockables
             var r = sut.UtcNow;
             r.Offset.Should().Be(TimeSpan.Zero);
         }
-
-        [Fact]
-        public void ShouldAlwaysFail()
-        {
-            false.Should().BeTrue();
-        }
     }
 }
